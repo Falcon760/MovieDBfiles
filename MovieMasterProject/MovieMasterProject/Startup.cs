d6cec2258ed0eb5760d077d11db76113c9ad88dc
Jon Camunag <jcamunag@gmail.com>
2014-08-04 13:36:51 -0400

    New Master Project Created

---
+++ MovieMasterProject/MovieMasterProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MovieMasterProject.Startup))]
+namespace MovieMasterProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
