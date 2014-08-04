48984db176d00b57ada0037b12b8c53f5a9f3620
Jon Camunag <jcamunag@gmail.com>
2014-08-04 11:22:13 -0400

    Created Initial/Brainstorm/Test Project
    Temporary project for testing.

---
+++ MovieTest/MovieTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MovieTest.Startup))]
+namespace MovieTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
