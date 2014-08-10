e631e664f3bb418999cd5a4001dd2555a767bdbd
Falcon760 <sfalcon266@hotmail.com>
2014-08-10 13:43:00 -0400

    customized navbar here

--- MovieMasterProject/MovieMasterProject/Director.cs
+++ MovieMasterProject/MovieMasterProject/Director.cs
@@ -11,6 +11,8 @@ namespace MovieMasterProject
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
     
     public partial class Director
     {
@@ -18,12 +20,15 @@ namespace MovieMasterProject
         {
             this.Movies = new HashSet<Movie>();
         }
-    
+
         public int DirectorId { get; set; }
+        [DisplayName("Director Name")]
         public string DirectorName { get; set; }
+        [DisplayName("Birth Date")]
+        [DisplayFormat(DataFormatString = "{0:MMM dd, yyyy}", ApplyFormatInEditMode = true)]
         public string DateOfBirth { get; set; }
         public string Bio { get; set; }
-    
+
         public virtual MessageBoardD MessageBoardD { get; set; }
         public virtual ICollection<Movie> Movies { get; set; }
     }
--- MovieMasterProject/MovieMasterProject/Rating.cs
+++ MovieMasterProject/MovieMasterProject/Rating.cs
@@ -11,12 +11,16 @@ namespace MovieMasterProject
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
     
     public partial class Rating
     {
         public int RatingId { get; set; }
         public string UserName { get; set; }
         public Nullable<int> MovieId { get; set; }
+        [DisplayName("Rating")]
+       [Range(0, 5, ErrorMessage = "Value must be between 0 and 5.")]
         public Nullable<decimal> Value { get; set; }
     
         public virtual Movie Movie { get; set; }
